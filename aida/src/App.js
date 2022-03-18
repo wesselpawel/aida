3265b4c90251732b9bfffcd7f0727145dcff45db
Paweł <wesselpawel@wp.pl>
2022-03-18 21:56:49 +0100

    homepage styling

--- aida/src/App.js
+++ aida/src/App.js
@@ -1,15 +1,12 @@
 import './styles/styles.scss';
-import logo from './images/Aidalogo3.png'
+import logo from './images/Aidalogo.png'
 function App() {
   return (
-    <div>
+    <div className="all">
+    <div className='filter'></div>
+      <div>
       <header>
         <ul>
-          <li className='first-item'>
-            <div>
-              <img src={logo} alt=""/>
-            </div>
-          </li>
           <li className='second-item'>
             <nav>
               <div>OFERTA</div>
@@ -19,6 +16,12 @@ function App() {
           </li>
         </ul>
       </header>
+      <div className='content'>
+        <div className='text'>
+          Firma to nie nazwa, stos papierów i zera na koncie. To mieszanka ludzkich doświadczeń, relacji i zaangażowania. Za każdym biznesem stoi człowiek i to właśnie do niego kieruję swoją ofertę.
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
